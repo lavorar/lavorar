a683bada2fd84d0e62d030fcb501bd782b25041e
frubilar1986 <rubilarfrancisco86@gmail.com>
2022-10-24 17:53:56 -0300

    select anidados

--- front/src/components/layout/elements/HomeSearchBar.jsx
+++ front/src/components/layout/elements/HomeSearchBar.jsx
@@ -35,87 +35,24 @@ const HomeSearchBar = (props) => {
           />
         </div>
         <div className="flex justify-center md:border-0 md:rounded-none w-full md:w-1/2 h-full md:h-[60%] border-gray-900 rounded-full dark:border-white-ghost border md:mr-2 text-black dark:text-white-ghost">
-        <SelectList
-        title="Provincia"
-        data={dataJson}
-        handleChange={(e) => {
-          setState(e.target.value);
-        }}
-
-      />     
-          {/* <input
-            id="searchLocation"
-            type="text"
-            className="w-11/12 overflow-clip h-full  bg-transparent outline-none "
-            placeholder="Ubicacion"
-          /> */}
-          {/* <select
-            className="w-11/12 overflow-clip h-full  bg-transparent outline-none"
-            name=""
-            placeholder="ubicacion"
-          >
-            <option
-              className="text-slate-800 disabled selected"
-              value="outline-none"
-            >
-              Provicia
-            </option>
-
-            {dataJson.map((getCountry, index) => (
-              <option
-                key={index}
-                className="bg-slate-600 text-left hover:bg-slate-800"
-                value="1"
-              >
-                {" "}
-                {getCountry.nombre}{" "}
-              </option>
-            ))}
-          </select> */}
+          <SelectList
+            title="Provincia"
+            data={dataJson}
+            handleChange={(e) => {
+              setState(e.target.value);
+            }}
+          />
         </div>
         <div className="flex justify-center md:border-0 md:rounded-none w-full md:w-1/2 h-full md:h-[60%] border-gray-900 rounded-full dark:border-white-ghost border md:mr-2 text-black dark:text-white-ghost">
-          
-        {state && <SelectList
-      title="Ciudad"
-      data={availableTags[0]}
-      handleChange={(e) => {
-        setTown(e.target.value);
-      }}
-      
-      
-      />}
-          
-          
-          {/* <input
-
-            id="searchLocation"
-            type="text"
-            className="w-11/12 overflow-clip h-full  bg-transparent outline-none "
-            placeholder="Ubicacion"
-          /> */}
-          {/* <select
-            className="w-11/12 overflow-clip h-full  bg-transparent outline-none"
-            name=""
-            placeholder="ubicacion"
-          >
-            <option
-              className="text-slate-800 disabled selected"
-              value="outline-none"
-            >
-              ciudad-localidad
-            </option>
-
-            {dataJson.map((getCountry, index) => (
-              <option
-                key={index}
-                className="bg-slate-600 text-left hover:bg-slate-800"
-                value="1"
-              >
-                {" "}
-                {getCountry.nombre}{" "}
-              </option>
-            ))}
-          </select> */}
+          {state && (
+            <SelectList
+              title="Ciudad"
+              data={availableTags[0]}
+              handleChange={(e) => {
+                setTown(e.target.value);
+              }}
+            />
+          )}
         </div>
         <div className="flex space-x-12 items-center w-2/3 justify-center h-full ml-2 md:w-52 bg-orange-brand rounded-full">
           <button type="submit" className=" text-3xl font-medium text-white">
