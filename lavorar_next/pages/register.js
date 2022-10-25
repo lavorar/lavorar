caaaacc2ca3e4f76b3792e7e24c643e3bb3a0c8f
santiagoavilez <santiagolingreed@gmail.com>
2022-10-25 15:12:28 -0300

    register back fix when islender is null

--- lavorar_next/pages/register.js
+++ lavorar_next/pages/register.js
@@ -12,15 +12,16 @@ import LastStep from '../components/SingUp/LastStep';
 import FormCompleted from '../components/SingUp/FormCompleted';
 const Register = (jwt) => {
     const { user } = useFetchUser();
-    const [formStep, setFormStep] = useState(2);
+    const [formStep, setFormStep] = useState(0);
     
     const nextFormStep = () => setFormStep((currentStep) => currentStep + 1);    
     const prevFormStep = () => setFormStep((currentStep) => currentStep - 1);
+    const back2FormStep = () => setFormStep((currentStep) => currentStep - 2);
     return (
         <SignUpLayout user={user}>
             <div className=" w-full min-h-screen justify-between">
                 <div className="w-full p-2 md:max-w-md md:mx-auto">
-                    <Progress currentStep={formStep} prevFormStep={prevFormStep} >                        
+                    <Progress currentStep={formStep} prevFormStep={prevFormStep} back2FormStep={back2FormStep} >                        
                         {formStep >= 0 && (
                             <RegisterComponent formStep={formStep} nextFormStep={nextFormStep} s />
                         )}
