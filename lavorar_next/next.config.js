3bfad3ac1c7817348e9ad1d73b607d8f394b242e
santiagoavilez <santiagolingreed@gmail.com>
2022-11-20 13:48:28 -0300

    fix babel

--- lavorar_next/next.config.js
+++ lavorar_next/next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve('next/babel')],
+    },
+  },
   images: {
     loader: 'cloudinary',
     path: 'https://res.cloudinary.com/dozjn0kxw/image/upload'
